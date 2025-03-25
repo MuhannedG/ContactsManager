26c50a87482098425bbc61fed7371377a1c3878e
Ahmad Bourhan <ahmadmghbourhan@gmail.com>
2025-03-25 16:14:54 +0000

    Created Home page and set as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+
   resources :groups
   resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
