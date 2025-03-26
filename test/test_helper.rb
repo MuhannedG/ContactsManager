12af641c8ec44d3968c9ae7c82c1eeffd146838a
MuhannedG <malek.gsees@hotmail.com>
2025-03-26 18:09:04 +0000

    fixing the test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,23 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "database_cleaner/active_record"
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors, with: :threads)
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+  # Configure DatabaseCleaner to use transaction strategy
+  DatabaseCleaner.strategy = :transaction
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
   end
 end
