09cfdad0b066c804ba3f5797987d056813cd65a0
MuhannedG <malek.gsees@hotmail.com>
2025-03-26 17:28:18 +0000

    tests updates

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    sign_in @user
     @contact = contacts(:one)
   end
 
